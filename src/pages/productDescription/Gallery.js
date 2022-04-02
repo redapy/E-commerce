32f9e75cf6f20be61867cadedb96660ddbce1b50
redapy <baha.redaaaa@gmail.com>
2022-04-02 14:19:14 +0000

    Gallery component: add functionality to change the main image depending on the thumbnail clicked

--- src/pages/productDescription/Gallery.js
+++ src/pages/productDescription/Gallery.js
@@ -3,19 +3,32 @@ import { Component } from "react";
 import { MainImage, Thumbnails, Wrapper } from "./Gallery.styles";
 
 class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: this.props.gallery[0],
+    };
+  }
+
+  changeImage = (src) => {
+    this.setState({
+      image: src,
+    });
+  };
   render() {
     const { gallery, alt } = this.props;
+    const { image } = this.state;
     return (
       <Wrapper>
         <Thumbnails>
           {gallery.map((imageSrc) => (
-            <button key={imageSrc}>
+            <button key={imageSrc} onClick={() => this.changeImage(imageSrc)}>
               <img src={imageSrc} alt={alt} />
             </button>
           ))}
         </Thumbnails>
         <MainImage>
-          <img src={gallery[0]} alt={alt} />
+          <img src={image} alt={alt} />
         </MainImage>
       </Wrapper>
     );
--- src/pages/productDescription/ProductDescription.styles.js
+++ src/pages/productDescription/ProductDescription.styles.js
@@ -6,6 +6,7 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
 `;
 
 export const ErrorMessage = styled.p`
