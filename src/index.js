3ff97ab96bc1e428afdf3795d8299caaacbc5950
redapy <baha.redaaaa@gmail.com>
2022-04-02 13:24:49 +0000

    configure the InMemoryCach to avoid having same attributes for different products

--- src/index.js
+++ src/index.js
@@ -16,7 +16,20 @@ const store = createStore(rootReducer);
 //connect react with with the GrapghQL endpoint
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    // Configure the generated cachedID that Appolo provide to avoid having the same cachedID for different products,
+    // which results having the same attriubtes for different product
+    //example for "AttributeSet": default generated cachedID is AttributeSet:id. Because the id's for each AttributeSet in the GraphQL
+    //schema are not unique, it will results in same cashedID generated. That's why I'm using items as a unique identifier.
+    typePolicies: {
+      AttributeSet: {
+        keyFields: ["items"],
+      },
+      Attribute: {
+        keyFields: ["id", "value"],
+      },
+    },
+  }),
 });
 
 ReactDOM.render(
